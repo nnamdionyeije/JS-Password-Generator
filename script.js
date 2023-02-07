16c335c28b73d1ebb3223852c740761e6d473c7f
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-02-07 17:13:26 -0500

    First additions to JavaScript code

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// function getLength() {
+//   var length = prompt("Input password length between 8 and 128 characters");
+
+//   if (isNaN(length)) {
+//     alert("Input must be a valid number");
+//   } else if (length < 8 || length > 128) {
+//     alert("Input must be a valid number");    
+//   }
+// }
+
+function generatePassword() {
+  
+  var length = prompt("Input password length between 8 and 128 characters");
+  var specChar = false;
+  var numChar = false;
+  var lowCase = false;
+  var upCase = false;
+
+  if (isNaN(length)) {
+    alert("Input must be provided as a number");
+    //find a way to have this line redirect the user back up to line 28
+  } else if (length < 8 || length > 128) {
+    alert("Input must be a valid number");    
+    //find a way to have this line redirect the user back up to line 28
+
+  }
+
+  specChar = confirm("Click OK to confirm including special characters.");
+  numChar = confirm("Click OK to confirm including numeric characters.");
+  lowCase = confirm("Click OK to confirm including lowercase characters.");
+  upCase = confirm("Click OK to confirm including uppercase characters.");
+
+  
+  
+  
+  
+  
+  
+
+}
